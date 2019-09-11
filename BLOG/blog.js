f234f502489d223a8930e797190c884f8af4746e
Jerem08 <j.vion@codeur.online>
2019-09-11 13:53:23 +0200

    Change of file

---
+++ BLOG/blog.js
@@ -0,0 +1,21 @@
+/* >>>>>>>>>> SMOOTH SCROLL <<<<<<<<<< */
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+/* >>>>>>>>>> PROGRESS BAR <<<<<<<<<< */
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160,
+  gutter: 20
+});
