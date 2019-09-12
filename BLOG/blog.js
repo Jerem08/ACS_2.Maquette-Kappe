60ddcbfa854880be936f8f92336f326806f7defc
Jerem08 <j.vion@codeur.online>
2019-09-12 16:57:49 +0200

    redesign of the site

--- BLOG/blog.js
+++ BLOG/blog.js
@@ -1,16 +1,8 @@
-/* >>>>>>>>>> SMOOTH SCROLL <<<<<<<<<< */
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+// external js: isotope.pkgd.js
 
-/* >>>>>>>>>> PROGRESS BAR <<<<<<<<<< */
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  masonry: {
+    columnWidth: 0
+  }
+});
