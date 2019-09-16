f2cf52bdc9fd6196e7b85f53f629ce41e9bc1ff3
Jerem08 <j.vion@codeur.online>
2019-09-16 14:38:04 +0200

    change files folder CONTACT and ABOUT

--- ABOUT/about.js
+++ ABOUT/about.js
@@ -1,4 +1,3 @@
-
 // sidebar
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -7,3 +6,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+// When the user clicks on div, open the popup
+
+$(".popup").click(function(){
+  $("#myPopup").addClass("show");
+});
+$(".popup").hover("",function(){
+  $("#myPopup").removeClass("show");
+});
