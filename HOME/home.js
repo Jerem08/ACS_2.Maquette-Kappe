7b48b953e2fb5bed34faaa2b1a32e196273ef8cc
blothe <b.lothe@codeur.online>
2019-09-16 13:53:06 +0200

    Add SIDEBAR to HOME

--- HOME/home.js
+++ HOME/home.js
@@ -12,3 +12,21 @@ var msnry = new Masonry( elem, {
 var msnry = new Masonry( '.grid', {
   // options
 });
+
+// sidebar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// When the user clicks on div, open the popup
+
+$(".popup").click(function(){
+  $("#myPopup").addClass("show");
+});
+$(".popup").hover("",function(){
+  $("#myPopup").removeClass("show");
+});
